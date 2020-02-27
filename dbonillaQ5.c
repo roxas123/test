0ee9c532da19b7932a613e72bb3639fa4da3b5b4
dbonilla13 <dbonilla13@csc208.csudh.edu>
2020-02-26 16:40:04 -0800

    loop question v2

--- dbonillaQ5.c
+++ dbonillaQ5.c
@@ -11,12 +11,15 @@ int main (void)
 {
 	double x = 0;
 	int i =0;
-
+	double temp;
 	for (i = 0; i <11; i++)
 	{
 		x = x +2;
 		x = x/20;
-		printf("%.3f \n",x);
+	
 	}
+	temp = x/20;
+	printf("%.3f",temp);
 	printf("The Value of x is %f \n", x);
+
 }
